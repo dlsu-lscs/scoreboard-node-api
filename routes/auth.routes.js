be37f91caf52383ade0d2ea45347fde1f96fcb69
Solenad <roe.dizon80@gmail.com>
2026-04-19 17:41:01 +0800

    feat: added api key validation for auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "../config/auth.js";
+import { validateApiKey } from "../middlewares/auth.middleware.js";
+
 import * as AuthController from "../controllers/auth.controller.js";
 
 const router = Router();
+
+router.use(validateApiKey);
 
 router.get("/api/auth/login-url", AuthController.getLoginUrl);
 
